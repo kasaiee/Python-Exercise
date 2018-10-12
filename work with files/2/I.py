098de35efd28ddb6b70f10df1b202b25f6a796e5
Mohammadreza Kasaiee <1tapciran@gmail.com>
2018-10-12 16:35:26 +0330

    Update I.py

--- work with files/2/I.py
+++ work with files/2/I.py
@@ -1,10 +1,21 @@
-lines = open('../words.txt').readlines()
-min_len = 1
+lines = open('words.txt').readlines()
+
+max_lengh = 1
 while True:
-    shortest_words = [word for word in lines if len(word) == min_len]
-    if shortest_words:
-        result = open('shortest-words.txt', 'w')
-        result.write(''.join(shortest_words))
-        break
-    else:
-        min_len += 1
+	res = [line for line in lines if len(line) == max_lengh]
+	if res:
+		break
+	else:
+		max_lengh += 1
+print(res)
+
+
+
+min_lengh = max([len(line) for line in lines])
+while True:
+	res = [line for line in lines if len(line) == min_lengh]
+	if res:
+		break
+	else:
+		max_lengh -= 1
+print(res)
